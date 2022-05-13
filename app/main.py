8ae2c5dcfc824027eb07283bf79dc9c95fa08780
vorph <klimczak.mathieu@pm.me>
2022-05-13 19:50:16 +0200

    initial commit

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from typing import Optional
+
+app = FastAPI(
+    title="Mathieu's API",
+    description="Simple API to be used as a docker tutorial",
+    version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get(
+    "/hello/",
+    tags=["hello"],
+    status_code=status.HTTP_200_OK,
+    response_description="Hello !",
+    summary="resume",
+)
+async def get_hello():
+    return {"Hello": "World"}
